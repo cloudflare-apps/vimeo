f8cfeee0f9ab93067cfd3459f2726e7c564a5f85
Connor Peshek <Connorpeshek@gmail.com>
2016-05-03 16:17:06 -0500

    add code

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
-(function () {
-  if (!window.addEventListener) return // Check for IE9+
-
+(function (){
+  let element
   let options = INSTALL_OPTIONS
 
-  function updateElement() {
+  const vimeoRegex = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)/i
+
+  function parseURL(url){
+    const match = vimeoRegex.exec(url)
+    let id
+
+    if (match){
+      id = match[3]
+    }
+    else {
+      if (!match) return null
+    }
+
+    return {id: id}
+  }
+
+  function add(){
+    for (let i = 0; i < options.embeds.length; i++){
+      if (!options.embeds[i].url || !options.embeds[i].location || !options.embeds[i].location.selector) return
+
+      const info = parseURL(options.embeds[i].url)
+
+      if (!info)
+        continue
+
+      let embed = `https://player.vimeo.com/video/${info.id}?`
+
+      if (options.embeds[i].autoplay){
+        embed += "autoplay=1&title=0&byline=0&portrait=0"
+      }
+      else {
+        embed += "title=0&byline=0&portrait=0"
+      }
+
+      element = Eager.createElement(options.embeds[i].location)
+      element.innerHTML = `<iframe src="${embed}" width="640" height="390" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>`
+    }
   }
 
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElement)
+    document.addEventListener("DOMContentLoaded", add)
   }
   else {
-    updateElement()
+    add()
   }
 
   window.INSTALL_SCOPE = {
     setOptions(nextOptions) {
       options = nextOptions
 
-      updateElement()
+      add()
     }
   }
 }())
--- build/app.js
+++ build/app.js
@@ -1,23 +1,56 @@
 "use strict";
 
 (function () {
-  if (!window.addEventListener) return; // Check for IE9+
-
+  var element = void 0;
   var options = INSTALL_OPTIONS;
 
-  function updateElement() {}
+  var vimeoRegex = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)/i;
+
+  function parseURL(url) {
+    var match = vimeoRegex.exec(url);
+    var id = void 0;
+
+    if (match) {
+      id = match[3];
+    } else {
+      if (!match) return null;
+    }
+
+    return { id: id };
+  }
+
+  function add() {
+    for (var i = 0; i < options.embeds.length; i++) {
+      if (!options.embeds[i].url || !options.embeds[i].location || !options.embeds[i].location.selector) return;
+
+      var info = parseURL(options.embeds[i].url);
+
+      if (!info) continue;
+
+      var embed = "https://player.vimeo.com/video/" + info.id + "?";
+
+      if (options.embeds[i].autoplay) {
+        embed += "autoplay=1&title=0&byline=0&portrait=0";
+      } else {
+        embed += "title=0&byline=0&portrait=0";
+      }
+
+      element = Eager.createElement(options.embeds[i].location);
+      element.innerHTML = "<iframe src=\"" + embed + "\" width=\"640\" height=\"390\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+    }
+  }
 
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElement);
+    document.addEventListener("DOMContentLoaded", add);
   } else {
-    updateElement();
+    add();
   }
 
   window.INSTALL_SCOPE = {
     setOptions: function setOptions(nextOptions) {
       options = nextOptions;
 
-      updateElement();
+      add();
     }
   };
 })();
